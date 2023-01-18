74d26d491d57a06f6a1c83fef950838b830b5e65
Giulio Occhipinti <occhipinti99@disroot.org>
2023-01-18 09:15:04 +0100

    first commit

---
+++ StreamBlock.js
@@ -0,0 +1,31 @@
+class StreamBlock {
+
+	constructor(measures, quantization)
+	{
+		this.measures = measures;
+		this.quantization = quantization;
+		this.nextArrow = 'R';
+		this.arrows = [];
+		this.lastPattern = null;
+	}
+  
+	addCommas()	// Puts the , every *quantization* arrows (e.g. every 16 arrows)
+	{
+		var finalStream = [];
+		var count = 0;
+
+		for (let i = 0; i < this.measures; i++)
+		{
+			for (let j = 0; j < this.quantization; j++)
+			{
+				finalStream.push(this.arrows[count++])
+			}
+
+			finalStream.push(',');
+		}
+
+		return finalStream;
+	}
+}
+
+module.exports = StreamBlock
---
+++ gen.js
@@ -0,0 +1,227 @@
+const StreamBlock = require('./StreamBlock.js');
+// Patterns that start with a foot, and the next one starts with the left arrow
+const nextArrowLeftPatterns = ['LDUR', 'LUDR', 'RUDUR', 'RDUDR', 'RUR', 'RDR']
+// Patterns that start with a foot, and the next one starts with the right arrow
+const nextArrowRightPatterns = ['RUDL', 'RDUL', 'LDUDL', 'LUDUL', 'LUL', 'LDL']
+// Patterns that start with left
+const startFromLeftPatterns = {'LDUR' : 29, 'LUDR' : 29, 'LDUDL' : 3, 'LUDUL' : 3, 'LUL' : 18, 'LDL' : 18}
+// Yeah
+const startFromRightPatterns = {'RUDL' : 29, 'RDUL' : 29, 'RUDUR' : 3, 'RDUDR' : 3, 'RUR' : 18, 'RDR' : 18 }
+
+const arrowsDict = {'L' : '1000', 'D' : '0100', 'U' : '0010', 'R' : '0001'}
+// Last N patterns (3 for now). If a pattern to be added is in this list, it is discarded
+var lastPatterns = ['X', 'X', 'X']	// Initialize with dummy values. Make part of StreamBlock?
+
+function generateStream(measures, quantization = 16)
+{
+	// Right now it only creates a list of arrows, will turn it to measures/, later, strem class etc
+	stream = new StreamBlock(measures, quantization);
+
+	// Need to generate measures * quantization arrows. Doesn't have to be precise, not a problem if it generates more than needed
+	arrowsToGenerate = measures * quantization;
+
+	addPattern(true, stream);
+
+	while (arrowsToGenerate > 0)
+	{
+		stream = addPattern(1, stream);	// if 0 candle, if 1 2 3 4 no
+		//stream = addPattern(Math.floor(Math.random() * 5), stream);	// if 0 candle, if 1 2 3 4 no
+		arrowsToGenerate -= stream.lastPattern.length;
+	}
+
+	console.log(stream);
+	return stream;
+}
+
+function addPattern(isNotCandle = true, stream)	// Passs stream object, keep "lastPattern" in stream class? TODO
+{
+	process.stdout.write("adding pattern ");
+	if (isNotCandle)
+	{
+		do {
+			pattern = chooseNextPattern((stream.nextArrow == 'L') ? startFromLeftPatterns : startFromRightPatterns)	
+		} while (lastPatterns.includes(pattern))
+	}
+
+	else	// isNotCandle is not zero
+	{
+		console.log("candle");
+		var secondToLastArrow = stream.lastPattern.slice(-2, -1)
+
+		if (secondToLastArrow == 'U')
+			stream.arrows.push(arrowsDict['D']);
+		
+		if (secondToLastArrow == 'D')
+			stream.arrows.push(arrowsDict['U']);
+		
+
+
+
+		/*if (nextArrowLeftPatterns.includes(stream.lastPattern))
+			pattern = chooseNextPattern(startFromRightPatterns)
+		
+		else if (nextArrowRightPatterns.includes(stream.lastPattern))
+			pattern = chooseNextPattern(startFromLeftPatterns)*/
+
+
+
+		
+		// It simply added the pattern without the check before, but resulted in mega luchis
+		/*if (nextArrowLeftPatterns.includes(stream.lastPattern))
+			dict = startFromRightPatterns;
+		
+		else if (nextArrowRightPatterns.includes(stream.lastPattern))
+			dict = startFromLeftPatterns;
+		
+		do {
+			pattern = chooseNextPattern(dict);	 
+		} while (lastPatterns.includes(pattern))*/
+
+		// TODO: TOO MANY DRILLS!!!!!!
+
+		if (nextArrowLeftPatterns.includes(stream.lastPattern))
+		{
+			do {
+				pattern = chooseNextPattern(startFromRightPatterns);
+				console.log(pattern[1]);
+			} while (pattern[1] == 'U' || lastPatterns.includes(pattern));
+			// I dont want that the chosen pattern has D as second arrow
+		}
+		
+		else if (nextArrowRightPatterns.includes(stream.lastPattern))
+		{
+			do {
+				pattern = chooseNextPattern(startFromLeftPatterns);
+			// I dont want that the chosen pattern has U as second arrow
+			} while (pattern[1] == 'D' || lastPatterns.includes(pattern));
+		}
+
+
+	}
+
+	stream.lastPattern = pattern;
+
+	lastPatterns.unshift(pattern);
+	lastPatterns.pop(); // Keep the list with the same amount of elements
+
+	console.log(pattern);
+
+	(pattern.slice(-1) == 'R') ? stream.nextArrow = 'L' : stream.nextArrow = 'R';
+
+	convertPatternToList(pattern).forEach(arrow => {
+		stream.arrows.push(arrow);
+	});
+	
+	return stream;
+}
+
+function chooseNextPattern(patternDict)
+{
+	console.log(patternDict)
+	var totalWeight = 100;
+	var num = Math.floor(Math.random() * (totalWeight - 1));
+
+	for (const [pattern, weight] of Object.entries(patternDict))
+	{
+		if (num < weight) return pattern;
+
+		num -= weight;
+	}
+}
+
+// Converts "LUDL" to ["1000", "0100", "0010", "0001"]
+function convertPatternToList(pattern)
+{
+	var list = [];
+
+	for (const arrow of pattern)
+	{
+		console.log(arrowsDict[arrow]);
+		list.push(arrowsDict[arrow])
+	}
+
+	return list;
+}
+
+
+function getNewChart(stream, streamBegin, streamEnd, inputLines)
+{
+	var outputLines = [];
+
+	streamLineCounter = 0
+	var isStreamAdded = false
+	console.log(stream.addCommas())
+
+	inputLines.forEach((line, i) => {
+		
+		if (i < streamBegin || i > (streamEnd + 1))
+		{
+			outputLines.push(line)
+		}
+
+		else if (!isStreamAdded)
+		{
+			// add stream
+			stream.addCommas().forEach((line, j) => {
+				outputLines.push(line)
+			});
+			isStreamAdded = true;
+		}
+	});
+
+	return outputLines.join("\n");
+	// turn outputlines into file, output.sm
+}
+
+function main()
+{
+	fs = require('fs');
+	// Input has 4 selected measures. starts at line 1744
+	chart = fs.readFileSync('./input.sm').toString();
+	lines = chart.split('\n');	// List of strings, each one is a line
+
+	var streamCounter = 0, streamBegin = 0, streamEnd = 0, insideStream = false;
+	lines.forEach((line, i) => {
+		
+		if (line == "2222")	// Start of quad hold
+		{
+			insideStream = true;
+			streamCounter += 1;
+			streamBegin = i;
+		}
+
+		if (insideStream)
+		{
+			console.log("processing line", line);
+
+			if (line == "0000")
+			{
+				streamCounter += 1;
+			}
+
+			if (line == "3333")
+			{
+				streamEnd = i;
+				insideStream = false;
+			}
+		}
+	});
+
+	streamEnd -= 1 // To avoid contaminating next measure (in python, not sure here)
+
+	measures = streamCounter / 4;
+
+	console.log("counter : ", streamCounter)
+	console.log("start st: ", streamBegin)
+	console.log("end strm: ", streamEnd)
+	console.log("measures: ", measures)
+
+	stream = generateStream(measures, 16)	// Generate n measures of 16ths
+
+	fs.writeFileSync('output.sm', getNewChart(stream, streamBegin, streamEnd, lines));
+}
+  
+if (require.main === module)
+{
+	main();
+}
