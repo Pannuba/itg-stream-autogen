a521c9d8214a4d402456f2c972da9a06a542c1bd
Giulio Occhipinti <giulio.occhipinti@azimutdirect.com>
2023-01-18 13:47:25 +0100

    create arrows dictionary

--- streamgen.py
+++ streamgen.py
@@ -1,27 +1,28 @@
 import random
 
 # TODO: dict, where the value is the priority of the pattern? (e.g. stairs have higher priority than doritos)
-# TODO: dict like ('L', "1000\n"; 'U', "0010\n"; ...)
 
 # Patterns that start with a foot, and the next one starts with the left arrow
-nextArrowLeftPatterns = ["LDUR", "LUDR", "RUDUR", "RDUDR", "RUR", "RDR"]
+nextArrowLeftPatterns = ['LDUR', 'LUDR', 'RUDUR', 'RDUDR', 'RUR', 'RDR']
 # Patterns that start with a foot, and the next one starts with the right arrow
-nextArrowRightPatterns = ["RUDL", "RDUL", "LDUDL", "LUDUL", "LUL", "LDL"]
+nextArrowRightPatterns = ['RUDL', 'RDUL', 'LDUDL', 'LUDUL', 'LUL', 'LDL']
 # Patterns that start with left
-startFromLeftPatterns = ["LDUR", "LUDR", "LDUDL", "LUDUL", "LUL", "LDL"]
+startFromLeftPatterns = ['LDUR', 'LUDR', 'LDUDL', 'LUDUL', 'LUL', 'LDL']
 # Yeah
-startFromRightPatterns = ["RUDL", "RDUL", "RUDUR", "RDUDR", "RUR", "RDR"]
+startFromRightPatterns = ['RUDL', 'RDUL', 'RUDUR', 'RDUDR', 'RUR', 'RDR']
+
+arrowsDict = {'L' : '1000\n', 'D' : '0100\n', 'U' : '0010\n', 'R' : '0001\n'}
 
 
 def createStream(streamLength):
 	
 	currentDirection = 'L';	# The next pattern must start with this arrow
-	stream = ""
+	stream = ''
 	firstArrowDone = 0
 
 	while (streamLength > 3):
 
-		print(currentDirection + " " + str(firstArrowDone))
+		print(currentDirection + ' ' + str(firstArrowDone))
 
 		match random.randint(0, 2):
 
@@ -31,9 +32,9 @@ def createStream(streamLength):
 					secondToLastArrow = pattern[-2]
 					
 					if secondToLastArrow == 'U':
-						stream += "0100\n"
+						stream += arrowsDict['D']
 					if secondToLastArrow == 'D':
-						stream += "0010\n"
+						stream += arrowsDict['U']
 
 					if pattern in nextArrowLeftPatterns:
 						pattern = startFromRightPatterns[random.randint(0, 5)]
@@ -64,44 +65,36 @@ def createStream(streamLength):
 				stream += convertPatternToRows(pattern)
 				firstArrowDone = 0
 	
-	print("stream:\n" + stream)
+	print('stream:\n' + stream)
 	return stream
 
 
 # Converts the pattern strings (LUDL, etc) in a row to be added to the .sm file
 def convertPatternToRows(pattern):
-	lines = ""
-	print("pattern = " + pattern)
+	lines = ''
+	print('pattern = ' + pattern)
 
 	for arrow in pattern:
-
-		if arrow == 'L':
-			lines += "1000\n"	# TODO: arrowDict.LEFT
-		if arrow == 'D':
-			lines += "0100\n"
-		if arrow == 'U':
-			lines += "0010\n"
-		if arrow == 'R':
-			lines += "0001\n"
+		lines += arrowsDict[arrow]
 	
 	return lines
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-	chart = open("chart.sm", "r+")
+	chart = open('chart.sm', 'r+')
 	streamblocks = [] # Line numbers where the streams begin?
 	# OR store a pair of (beginningLine, #notes)
 	# TODO: Make class for a block of stream?
 	lineCounter = 0; streamBegin = 0; streamCounter = 0; streamEnd = 0
 
 	# TODO: Dict like (X, Y) where X is the line # where a stream block starts, Y is for how many rows or arrows
-	# Ignore commas? or keep count somewhere, or "learn" what type of stream it is by counting how many arrows are
+	# Ignore commas? or keep count somewhere, or 'learn' what type of stream it is by counting how many arrows are
 	# between two commas and assume it's like that for the whole duration
 	for i, line in enumerate(chart): # Read line given line #??
 		lineCounter += 1
 
-		if line == "XXXX\n":	# For now only 1 stream
+		if line == 'XXXX\n':	# For now only 1 stream
 			streamCounter += 1
 
 			if streamBegin == 0:	# TODO: Avoid this check every time, only required at the beginning of the stream. Probably already done
@@ -109,7 +102,7 @@ if __name__ == "__main__":
 
 			for j, liine in enumerate(chart):
 
-				if liine == "XXXX\n":
+				if liine == 'XXXX\n':
 					streamCounter += 1
 				
 				else:
